2b8268e85fd38a92e5d2e558f443fdc8e0df364b
DESKTOP-A2TR5DO\Jungho <luckjungho@gmail.com>
2020-02-29 23:02:47 +0900

    QuestFragment 에 유저 정보 뷰 추가

--- app/src/main/java/com/zs/mol/view/quest/fragment/QuestFragment.kt
+++ app/src/main/java/com/zs/mol/view/quest/fragment/QuestFragment.kt
@@ -7,11 +7,15 @@ import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import com.zs.mol.R
+import com.zs.mol.model.db.inventory.Inventory
 import com.zs.mol.model.quest.Quest
+import com.zs.mol.model.quest.RewardKey
+import com.zs.mol.model.user.UserManager
 import com.zs.mol.view.base.MainFragment
 import com.zs.mol.view.quest.viewmodel.QuestViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.fragment_quest.*
+import kotlinx.android.synthetic.main.view_user_status.view.*
 
 class QuestFragment : MainFragment() {
     val viewModel = QuestViewModel()
@@ -30,8 +34,17 @@ class QuestFragment : MainFragment() {
     }
 
     fun init() {
+        initUserStatusView()
         eventButton.setOnClickListener { onClickNewQuest() }
         refreshButton.setOnClickListener { onClickRefresh() }
+    }
+
+    private fun initUserStatusView() {
+        UserManager.user?.apply {
+            userStatusView.level.text = "Lv. ${userStatus.level}"
+            userStatusView.nickname.text = id
+            userStatusView.gold.text = "${Inventory.getAmount(RewardKey.GOLD)} G"
+        }
     }
 
     private fun onClickNewQuest() {
